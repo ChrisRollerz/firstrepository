0f1d1a567b3c8fe5f5d74b5b904413790b7442c3
ChrisRollerz <32064645+ChrisRollerz@users.noreply.github.com>
2017-10-08 14:24:27 +0100

    Hello World
    added seconds to time

--- Hello World.py
+++ Hello World.py
@@ -6,6 +6,6 @@ print("How are you?")
 
 from datetime import datetime
 date_object = datetime.now()
-current_time = date_object.strftime('%H:%M')
+current_time = date_object.strftime('%H:%M:%S')
 print("The time is:")
 print(current_time)
