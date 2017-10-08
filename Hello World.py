681f743a062a379453584cddbdebffc5516e8294
ChrisRollerz <32064645+ChrisRollerz@users.noreply.github.com>
2017-10-08 14:19:37 +0100

    Add files via upload
    Added time

--- Hello World.py
+++ Hello World.py
@@ -2,3 +2,10 @@ print("Hello World")
 name=input("What is your name? ")
 print("Hi, ");
 print(name)
+print("How are you?")
+
+from datetime import datetime
+date_object = datetime.now()
+current_time = date_object.strftime('%H:%M')
+print("The time is:")
+print(current_time)
